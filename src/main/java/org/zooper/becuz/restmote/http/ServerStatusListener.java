fe79d18496a304acc3fefd254b2fd86369d38998
bebo <albertopierini@gmail.com>
2012-11-26 15:35:20 +0100

    javadoc in listener

--- src/main/java/org/zooper/becuz/restmote/http/ServerStatusListener.java
+++ src/main/java/org/zooper/becuz/restmote/http/ServerStatusListener.java
@@ -1,7 +1,15 @@
 package org.zooper.becuz.restmote.http;
 
+/**
+ * Listener of {@link Server} status  
+ * 
+ * @author bebo
+ */
 public interface ServerStatusListener {
 
+	/**
+	 * @param running server running
+	 */
 	public void serverStatusChanged(boolean running);
 	
 }
